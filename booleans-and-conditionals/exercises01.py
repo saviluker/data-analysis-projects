7fecdfbc166cb5095675c2ee37a07209e63db883
Savannah Luker <saviluker@gmail.com>
2025-11-02 12:36:14 -0600

    Complete booleans and conditionals exercise 1

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,12 +1,18 @@
 # Assign the variables for exercise 1 here:
-
+engine_indicator_light = "red blinking"
+space_suits_on = True
+shuttle_cabin_ready = True
+crew_status = space_suits_on and shuttle_cabin_ready
+computer_status_code = 200
+shuttle_speed = 15000
 
 
 # BEFORE running the code, predict what will be printed to the console by the following statements:
-
+#I predict that it will state "engines are off"
 if engine_indicator_light == "green": 
   print("engines have started")
 elif engine_indicator_light == "green blinking": 
   print("engines are preparing to start")
 else:
   print("engines are off")
+  
