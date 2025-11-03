a38d13762e5752b41a759aa8fcc092da535d75c7
Savannah Luker <saviluker@gmail.com>
2025-11-03 12:43:57 -0600

    Complete for-loop-exercise.py

--- loops/exercises/for-loop-exercises.py
+++ loops/exercises/for-loop-exercises.py
@@ -1,8 +1,20 @@
 # Exercise #1: Construct for loops that accomplish the following tasks:
-    # a. Print the numbers 0 - 20, one number per line.
-    # b. Print only the ODD values from 3 - 29, one number per line.
-    # c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
-    # d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
 
+    # a. Print the numbers 0 - 20, one number per line.
+for num in range(21):
+    print(num)
+
+    # b. Print only the ODD values from 3 - 29, one number per line.
+for num in range(3, 30, 2):
+    print(num)
+
+    # c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
+for num in range(12, -15, -2):
+    print(num)
+
+    # d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
+for num in range (50, 20, -3):
+    print(num)
+    #stumped on this one!
 
 
