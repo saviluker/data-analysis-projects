6ef1f920606f39f6184e4f44d5cae9f2fc6e19c2
Savannah Luker <saviluker@gmail.com>
2025-11-06 12:19:04 -0600

    Python Exercise_Strings.py

--- collections/exercises/strings.py
+++ collections/exercises/strings.py
@@ -4,22 +4,43 @@ text = 'Strings_are_sequences_of_characters.'
 word = 'tomato'
 
 # 1. Print a slice of the first 12 characters from 'text'.
+new_string = text[0: 13]
+print(new_string)
+#Strings_are_s
 
 
 # 2. Print a slice of the last 12 characters from 'text'. You should NOT have to count the index values yourself!
+length_of_text = len(text)
+print(length_of_text)
+#36 characters long
 
+new_string = text[24: 37]
+print(new_string)
+#_characters.
 
 # 3. Print a slice of the middle 12 characters from 'text'.
-
+new_string = text[13: 25]
+print(new_string)
+#equences_of_
 
 # ---- Exercise 3: Looping Through a String ----
 
 # Use index values to loop backwards through 'word'.
 
 # 1. Print 1 letter per line.
-
+max_index = len(word)-1
+for index in range(max_index, -1, -1):
+    print(word[index])
 
 # 2. Refactor the code to use the accumulator pattern to build up and print the reversed string. For example, if given 'good', print 'doog' on one line.
+new_word = 'flower'
+for index in range(max_index, -1, -1):
+    new_word += word[index]
 
-
+print(new_word)
 # 3. Refactor the code to print a combination of the original and reversed string. For example, given 'tomato', print 'tomatootamot'. (If you want to be fancy, print 'tomato | otamot').
+new_word = 'tomato'
+for index in range(max_index, -1, -1):
+    new_word += word[index]
+
+print(new_word)
